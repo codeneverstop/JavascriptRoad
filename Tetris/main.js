6315b9d81277996dfee2e6d8d59477bbde05889a
codeneverstop <amecoderwang@gmail.com>
2019-04-18 01:28:21 +0800

    why collide not use forEach
    在forEach里break是没有用的，因为这里forEach是个函数，是肯定都要执行的。 所以这里用forEach不合适。

--- Tetris/main.js
+++ Tetris/main.js
@@ -58,24 +58,30 @@ function merge(arena, player){
 /*判断对于arena来说 player的输入是外界输入，需要判断合法性*/
 function collide(arena, player){
 	const [pos, matrix] = [player.pos, player.matrix];
+	
 	/*
+	collide_flag = false;
 	matrix.forEach((row, y) => {
 		row.forEach((value, x) => {
 			//if (arena[y + pos.y][x + pos.x] !== 0)
-			console.log("pos origin(" + y + ", " + x + "), matrix pos is " + matrix[y][x]);
-			console.log("pos real(" + (pos.y + y) + ", " + (pos.x + x) +"), arena real pos is " + arena[y + pos.y][x + pos.x]);
+			console.log("+pos origin(" + y + ", " + x + "), matrix pos is " + matrix[y][x]);
+			//console.log("pos real(" + (pos.y + y) + ", " + (pos.x + x) +"), arena real pos is " + arena[y + pos.y][x + pos.x]);
 			if (matrix[y][x] !== 0)
 			{
-				if(!arena[y + pos.y] || (arena[y + pos.y][x + pos.x] !== 0))
+				if((arena[y + pos.y] && arena[y + pos.y][x + pos.x]) !== 0)
 				{
-					return true;
+					console.log("+collied");
+					collide_flag = true;  //在这里break是没有用的，因为这里forEach是个函数，是肯定都要执行的。 所以这里用forEach不合适。
 				}
 			}
 		});
 	});
+	return collide_flag;
 	*/
+	
 	for (let y = 0; y < matrix.length; ++y){
 		for (let x = 0; x < matrix[y].length; ++x){
+			console.log("-pos origin(" + y + ", " + x + "), matrix pos is " + matrix[y][x]);
 			if (matrix[y][x] !== 0 && 
 				(arena[y + pos.y] && 
 				arena[y + pos.y][x + pos.x]) !== 0){
@@ -83,7 +89,7 @@ function collide(arena, player){
 			}
 		}
 	}
-
+	
 	return false;
 }
 
@@ -123,8 +129,16 @@ function update(time = 0){ //默认参数 单位为ms new
 document.addEventListener('keydown', event => {
 	if (event.keyCode === 37){ // left
 		player.pos.x--;
+		if (collide(arena, player))
+		{
+			player.pos.x++;
+		}
 	} else if (event.keyCode === 39){
 		player.pos.x++;
+		if (collide(arena, player))
+		{
+			player.pos.x--;
+		}
 	} else if (event.keyCode === 40){
 		playerDrop();
 	}
